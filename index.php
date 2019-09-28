f4623de587b2b3f802ae684a301a506d9cfe87e8
Alexandr Varygin <otz-job@mail.ru>
2019-09-28 18:41:05 +0300

    use data as an array

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <?php
-$connection = new PDO ('mysql:host=localhost; dbname=marlin-php1; charset=utf8', 'root', '');
-$userData = $connection->query('SELECT * FROM `user_data`');
-$userData = $userData->fetchAll();
-$comments = $connection->query('SELECT * FROM `comments`');
-$comments = $comments->fetchAll();
-// var_dump($userData);
-// var_dump($comments);
+
+ $comments = [
+   ['user_id' => 1,
+      'user_name' => 'Chuck',
+      'user_surname' => 'Norris',
+      'user_img' => 'img/Chuck-Norris.jpg',
+      'user_comment' => 'comment 1',
+      'date_comment' => '2019-09-23'],
+
+   ['user_id' => 2,
+      'user_name' => 'Elon',
+      'user_surname' => 'Musk',
+      'user_img' => 'img/Elon-Musk.jpg',
+      'user_comment' => 'comment 2',
+      'date_comment' => '2019-09-24'],
+
+   ['user_id' => 3,
+      'user_name' => 'Janna',
+      'user_surname' => 'Badoeva',
+      'user_img' => 'img/no-user.jpg',
+      'user_comment' => 'comment 3',
+      'date_comment' => '2019-09-25']
+ ];
+
 ?>
 
 <!DOCTYPE html>
@@ -68,24 +85,16 @@ $comments = $comments->fetchAll();
 
                                <?php foreach ($comments as $comment) { ?> 
                                 <div class="media">
-                                  <img src="<?php 
-                                              $temp_user_img = $connection->query('SELECT user_img FROM `user_data` WHERE user_id='.$comment['user_id'].' ');
-                                              $temp_user_img = $temp_user_img->fetchAll();
-                                              echo $temp_user_img[0][0];
+                                  <img src="<?php echo $comment['user_img'];
                                             ?>" class="mr-3" alt="..." width="64" height="64">
                                   <div class="media-body">
                                     <h5 class="mt-0">
-                                     <?php 
-                                       $temp_user_name = $connection->query('SELECT user_name FROM `user_data` WHERE user_id='.$comment['user_id'].' ');
-                                       $temp_user_name = $temp_user_name->fetchAll();
-                                       $temp_user_surname = $connection->query('SELECT user_surname FROM `user_data` WHERE user_id='.$comment['user_id'].' ');
-                                       $temp_user_surname = $temp_user_surname->fetchAll();
-                                       echo $temp_user_name[0][0]. " " . $temp_user_surname[0][0];
+                                     <?php echo $comment['user_name']." ". $comment['user_surname'];
                                      ?>
                                     </h5>
-                                    <span><small><?php echo date('d/m/Y', strtotime($comment['comment_date'])); ?></small></span>
+                                    <span><small><?php echo date('d/m/Y', strtotime($comment['date_comment'])); ?></small></span>
                                     <p>
-                                       <?php echo $comment['comment_content']; ?>
+                                       <?php echo $comment['user_comment']; ?>
                                     </p>
                                   </div>
                                 </div>
