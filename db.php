0a1130f24413f97001fb35af00d3e65e3607c537
Alexandr Varygin <otz-job@mail.ru>
2019-11-02 16:06:03 +0300

    new options for PDO

--- db.php
+++ db.php
@@ -9,7 +9,11 @@
     $db_user = 'root';
     $db_pass = '';
     $charset = 'utf8';
-    $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false
+    ];
     $dsn = "$driver:host=$host;dbname=$db_name;charset=$charset";
 
     try{
